82ab979aa50b9aed6921c088c96cd38739b630ec
Alexey Slyusarenko <slyusarenko.a.n@gmail.com>
2018-10-01 22:58:18 +0300

    Test show video stream

--- js/blocks/video.js
+++ js/blocks/video.js
@@ -15,7 +15,6 @@ aspectRatio = widthVideoStreamElem > heightVideoStreamElem ?
 console.log(aspectRatio);
 videoSetting = {
         width: { min: 640, ideal: 1280, max: 1920 },
-        height: { min: 480, ideal: 720, max: 1080 },
         facingMode: 'environment',
         aspectRatio: 1 //aspectRatio
 };
